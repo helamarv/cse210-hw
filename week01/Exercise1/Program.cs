c1a2125389045f293c547fb53d8669e6db0182bf
Helama Vieira <helamarv@gmail.com>
2026-09-04 22:42:14 +0100

    Ex 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
-using System;
+    using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        
     }
 }
